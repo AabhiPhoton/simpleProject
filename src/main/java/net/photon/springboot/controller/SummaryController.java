dd94510330a6a62d853e90f0ccd239d6e8b20235
Abhijeet <abhijeet.p@photoninfotech.net>
2018-09-17 11:26:41 -0500

    added swagger Configuration and the Api documentation

--- src/main/java/net/photon/springboot/controller/SummaryController.java
+++ src/main/java/net/photon/springboot/controller/SummaryController.java
@@ -1,13 +1,18 @@
 package net.photon.springboot.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(value = "Summary", description = "Displays the welcome message and has rest calls for people summary")
 @RestController
 public class SummaryController {
 
 
-    @RequestMapping(value = "/")
+    @ApiOperation(value = "Performs Get operation and gives welcome message")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String firstMessage(){
         return "Welcome to Photon Spring Boot Training Example";
     }
